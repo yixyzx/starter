a0929d1d9c214300e760f8f678e9483b63e6e6f8
Opportunity <opp@YiXMars.yixlan>
2025-03-22 07:37:30 +0800

    opts

--- lua/options.lua
+++ lua/options.lua
@@ -14,3 +14,12 @@ vim.opt.fileencodings = "utf-8,ucs-bom,gb18030,gbk,gb2312,cp936"
 vim.opt.encoding = "utf-8"
 -- 将wrap选项设置为false，等同于在Vim中执行:set nowrap
 vim.opt.wrap = false
+
+-- tab --
+-- This setting determines the number of spaces that a tab character represents. By setting it to 4, Vim will insert 4 spaces whenever you press the tab key
+vim.opt.tabstop = 4
+-- This setting controls the number of spaces used for each step of indentation. Setting it to 4 ensures that your code is indented consistently
+vim.opt.shiftwidth = 4
+-- This option converts tabs to spaces. When enabled, pressing the tab key will insert spaces instead of a tab character
+vim.opt.expandtab = true
+
